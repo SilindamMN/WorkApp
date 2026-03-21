0f05a4e0de4bca1df8b9525d58841e0b8c121c1d
silindamn <silindanemdu@gmail.com>
2026-03-22 01:10:02 +0200

    up project id

--- WorkApp/Domain/Dtos/GeneralAdmin/ClientDetailDto.cs
+++ WorkApp/Domain/Dtos/GeneralAdmin/ClientDetailDto.cs
@@ -7,7 +7,9 @@
   using System.Threading.Tasks;
 
   public class ClientDetailDto
-  {
-    public string ProjectName { get; set; }
-  }
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime ProjectStartDate { get; set; }
+    }
 }
